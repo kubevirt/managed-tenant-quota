2fc9b7d2d1806614e1aeb8746820b29cf471dd86
Nahshon Unna-Tsameret <nunnatsa@redhat.com>
2023-09-18 17:12:02 +0300

    Protect the MTQ status
    The status should be defined as a subresource so it will requires its
    own permissions.
    
    Signed-off-by: Nahshon Unna-Tsameret <nunnatsa@redhat.com>

--- pkg/mtq-operator/controller.go
+++ pkg/mtq-operator/controller.go
@@ -96,7 +96,7 @@ func newReconciler(mgr manager.Manager) (*ReconcileMTQ, error) {
 		namespacedArgs: &namespacedArgs,
 	}
 	callbackDispatcher := callbacks.NewCallbackDispatcher(log, restClient, uncachedClient, scheme, namespace)
-	r.reconciler = sdkr.NewReconciler(r, log, restClient, callbackDispatcher, scheme, createVersionLabel, updateVersionLabel, LastAppliedConfigAnnotation, certPollInterval, finalizerName, false, recorder)
+	r.reconciler = sdkr.NewReconciler(r, log, restClient, callbackDispatcher, scheme, createVersionLabel, updateVersionLabel, LastAppliedConfigAnnotation, certPollInterval, finalizerName, true, recorder)
 
 	r.registerHooks()
 	return r, nil
@@ -151,7 +151,11 @@ func (r *ReconcileMTQ) Reconcile(_ context.Context, request reconcile.Request) (
 		return reconcile.Result{}, err
 	}
 
-	return r.reconciler.Reconcile(request, operatorVersion, reqLogger)
+	res, err := r.reconciler.Reconcile(request, operatorVersion, reqLogger)
+	if err != nil {
+		reqLogger.Error(err, "failed to reconcile")
+	}
+	return res, err
 }
 
 func (r *ReconcileMTQ) add(mgr manager.Manager) error {
